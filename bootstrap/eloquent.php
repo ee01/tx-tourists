1a9ef895d09253e913fdc477b800b3ed516c7e6f
Esone <01@eexx.me>
2024-07-17 09:54:24 +0800

    read db config from settings.php

--- bootstrap/eloquent.php
+++ bootstrap/eloquent.php
@@ -8,13 +8,15 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 return static function (App $app): void {
     // 初始化 Eloquent ORM
+    $container = $app->getContainer();
+    $settings = $container->get('settings');
     $capsule = new Capsule;
     $capsule->addConnection([
         'driver' => 'mysql',
-        'host' => env('DB_HOST'),
-        'database' => env('DB_DATABASE'),
-        'username' => env('DB_USERNAME'),
-        'password' => env('DB_PASSWORD'),
+        'host' => $settings['db']['host'],
+        'database' => $settings['db']['database'],
+        'username' => $settings['db']['username'],
+        'password' => $settings['db']['password'],
         'charset' => 'utf8',
         'collation' => 'utf8_unicode_ci',
         'prefix' => '',
