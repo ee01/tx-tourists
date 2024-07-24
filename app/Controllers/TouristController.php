fed442f552752ce0d2538dd16c4d6b821d7a79c1
Esone <01@eexx.me>
2024-07-24 15:48:54 +0800

    verify by wps

--- app/Controllers/TouristController.php
+++ app/Controllers/TouristController.php
@@ -26,11 +26,19 @@ final class TouristController extends Controller
 
     public function create(Request $request, Response $response, $args): Response
     {
-        // return $this->render($response, 'home/index.twig');
         $raw = $request->getParsedBody();
         $tourist = Tourist::create([
             'raw' => $raw
         ]);
         return $this->json($response, ['id' => $tourist->id]);
+    }
+    public function create_by_wps(Request $request, Response $response, $args): Response
+    {
+        $response = $this->create($request, $response, $args);
+        return $this->json($response, ['bind_code' => '20240420223823449467314']);
+    }
+    public function verify_by_wps(Request $request, Response $response, $args): Response
+    {
+        return $this->json($response, ['bind_code' => '20240420223823449467314']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,9 @@ return static function (Slim\App $app): void {
     $app->get('/', [HomeController::class, 'index'])->setName('home.index');
     $app->get('/about', [AboutController::class, 'index'])->setName('about.index');
     $app->get('/contact', [ContactController::class, 'index'])->setName('contact.index');
-    $app->get('/tourists', [ContactController::class, 'index']);
+    $app->get('/tourists', [TouristController::class, 'index']);
     $app->get('/tourists/{id}', [TouristController::class, 'get']);
     $app->post('/tourists', [ContactController::class, 'create']);
+    $app->post('/tourists/bywps', [ContactController::class, 'create_by_wps']);
+    $app->get('/tourists/bywps', [ContactController::class, 'verify_by_wps']);
 };
